14fd368a058f3278927a1de28f0ccd9708d3199f
quguiliang <quguiliang20@163.com>
2021-03-21 09:47:12 +0800

    HashMap Test

--- src/qgl/TestHashMap.java
+++ src/qgl/TestHashMap.java
@@ -7,6 +7,7 @@ public class TestHashMap {
     public static void main(String[] args) {
         Map<String,String> map = new HashMap<>();
         map.put("1","2");
+        map.put("2","3");
         System.out.println(map.values());
     }
 }
